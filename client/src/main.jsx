c13302b95be660a0650221950b994f0a2b20c0e3
edine-noella <edinenoella@gmail.com>
2025-01-19 17:01:54 +0200

    fix: fixing a deployment bug

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 
-import React from 'react';
 import 'regenerator-runtime/runtime';
+import React from 'react';
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App.jsx'
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy'; 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['regenerator-runtime/runtime']
-    }
-  },
-  optimizeDeps: {
-    include: ['regenerator-runtime/runtime']
-  }
+  plugins: [react(),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
+  ],
 })
